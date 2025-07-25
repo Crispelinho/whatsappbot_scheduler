bcbc91fa9247a2f616894593baf8a4c2b4406d37
Cristhian Carpio <cristhian.carpio18@gmail.com>
2025-07-24 21:55:11 -0500

    Se añade app clients

---
+++ whatsappbot_scheduler/clients/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    first_visit_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.full_name} ({self.phone_number})"

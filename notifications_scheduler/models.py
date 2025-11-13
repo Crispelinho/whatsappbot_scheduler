047ffe661f2363681bf48f8242a7e6633b599226
Crispelinho <cristhian.carpio18@gmail.com>
2025-11-13 21:56:59 +0100

    Feature/se anade cogido de area (#2)
    * fix: Corregir la ruta del archivo por lotes para iniciar Celery en Windows
    
    * feat: Se añade lógica para envío de video
    
    * Se añaden nuevos modelos para error types y admin
    
    * Se añaden tareas para reintentos y lógica en model y services
    
    * Se añaden tareas para reintentos y lógica en model y services
    
    * feat: Refactor WhatsApp message sending logic and enhance error handling
    
    * feat: Refactor WhatsApp message sending logic and enhance error handling
    
    * feat: Enhance WhatsApp message sending with error handling and file attachment support
    
    * feat: Enhance WhatsApp message sending with error handling and file attachment support
    
    * feat: Enhance message handling and retry logic with improved error management and file attachment support
    
    * feat: Enhance message handling and retry logic with improved error management and file attachment support
    
    * feat: Update admin models to enhance client message handling and response management
    
    * feat: Update admin models to enhance client message handling and response management
    
    * feat: Update message sending logic to include area code and enhance client message handling
    
    * feat: Update message sending logic to include area code and enhance client message handling
    
    * feat: Enhance message sending logic to improve error handling and response management
    
    * feat: Enhance message sending logic to improve error handling and response management
    
    * feat: Refactor appointment message creation and enhance error logging with area code
    
    * feat: Refactor appointment message creation and enhance error logging with area code
    
    * feat: Update models and services to use ResponseCode for error handling and improve admin registration
    
    ---------
    
    Co-authored-by: israel1947 <israeldavidsdvsf@gmail.com>

--- notifications_scheduler/models.py
+++ notifications_scheduler/models.py
@@ -68,6 +68,18 @@ class ScheduledMessage(models.Model):
     def __str__(self):
         return f"{self.subject} ({self.status})"
 
+
+class ErrorType(models.Model):
+    """Error types (e.g., Network, SocialNetworkSenderInterface Block, Invalid Client, etc.)"""
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=50, unique=True, choices=[(e.value, e.name) for e in ResponseCode])
+    description = models.TextField()
+    retryable = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
 class MessageResponse(models.Model):
     class Status(models.TextChoices):
         PENDING = "pending", "Pending"
@@ -80,9 +92,13 @@ class MessageResponse(models.Model):
         related_name='response'
     )
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING, db_index=True)
-    response_code = models.CharField(max_length=50, blank=True, null=True, choices=[(e.value, e.name) for e in ResponseCode])
-    description = models.TextField("Description", blank=True, null=True)
-    retryable = models.BooleanField(default=False)
+    error_type = models.ForeignKey(
+        ErrorType,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="responses"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -90,10 +106,10 @@ class MessageResponse(models.Model):
         return f"Response {self.id} - {self.status}"
 
     def clean(self):
-        if self.status == MessageResponse.Status.FAILED and not self.response_code:
-            raise ValidationError("You must assign a response_code if the status is 'failed'.")
-        if self.status in [MessageResponse.Status.PENDING, MessageResponse.Status.SENT] and self.response_code and self.response_code != ResponseCode.SUCCESS.value:
-            raise ValidationError("You cannot assign a non-success response_code if the status is not 'failed'.")
+        if self.status == MessageResponse.Status.FAILED and not self.error_type:
+            raise ValidationError("You must assign an error type if the status is 'failed'.")
+        if self.status in [MessageResponse.Status.PENDING, MessageResponse.Status.SENT] and self.error_type:
+            raise ValidationError("You cannot assign an error type if the status is not 'failed'.")
 
     def save(self, *args, **kwargs):
         self.full_clean()
@@ -121,20 +137,4 @@ class ClientScheduledMessage(models.Model):
         ]
 
     def __str__(self):
-        status = self.response.status if hasattr(self, "response") else "no response"
-        return f"Message to {self.client.full_name} - {status}"
-
-    @property
-    def can_retry(self):
-        """Determina si se puede reintentar el envío según el response_code y cantidad de retries."""
-        if not hasattr(self, "response") or not self.response.response_code:
-            return False
-        return (
-            self.response.response_code in [
-                ResponseCode.NETWORK.value,
-                ResponseCode.TIMEOUT.value,
-                ResponseCode.WHATSAPP_DOWN.value,
-                ResponseCode.RATE_LIMIT.value
-            ]
-            and self.retry_count < self.max_retries
-        )
+        return f"Message to {self.client.full_name} - {self.response.status}"
